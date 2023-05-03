df5a99272bf11eef634c28550537cd19f7c94f9e
Tucker Craig <tuckermcraig@gmail.com>
2023-05-03 13:19:45 -0400

    styles sign in and register forms

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -30,57 +30,87 @@
   }
 </script>
 
-<div class="container flex flex-col items-center justify-center flex-1">
-  <h1>{register ? "Register" : "Log In"}</h1>
-  <form class="flex flex-col">
-    <label>
-      <input bind:value={email} type="email" placeholder="Email" />
-    </label>
-    <label>
-      <input bind:value={password} type="password" placeholder="Password" />
-    </label>
-    {#if register}
-      <label>
-        <input
-          bind:value={confirmPassword}
-          type="password"
-          placeholder="Password"
-        />
+<div class="w-full max-w-xs">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div class="mb-4">
+      <h1>{register ? "Register" : "Log In"}</h1>
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Email
       </label>
+      <input
+        bind:value={email}
+        type="email"
+        placeholder="Email"
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      />
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input
+        bind:value={password}
+        type="password"
+        placeholder="Password"
+        class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+      />
+      {#if register}
+        <label>
+          <input
+            bind:value={confirmPassword}
+            type="password"
+            placeholder="Confirm Password"
+            class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </label>
+      {/if}
+      <p class="text-red-500 text-xs italic">Please choose a password.</p>
+    </div>
+    <div class="flex items-center justify-between">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="button"
+      >
+        {register ? "Register" : "Sign In"}
+      </button>
+      <div
+        class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+        href="/"
+        on:click={() => {
+          authHandlers.resetPassword(email);
+        }}
+        on:keydown={() => {}}
+      >
+        Forgot Password?
+      </div>
+    </div>
+    {#if register}
+      <div>
+        Already have an account?
+        <div
+          class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded w-fit text-sm"
+          on:click={() => {
+            register = false;
+          }}
+          on:keydown={() => {}}
+        >
+          Log In
+        </div>
+      </div>
+    {:else}
+      <div class="mt-5">
+        Don't have an account? <div
+          class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded w-fit text-sm"
+          on:click={() => {
+            register = true;
+          }}
+          on:keydown={() => {}}
+        >
+          Sign Up
+        </div>
+      </div>
     {/if}
-    <button
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      on:click={handleSubmit}>Submit</button
-    >
   </form>
-  {#if register}
-    <div
-      on:click={() => {
-        register = false;
-      }}
-      on:keydown={() => {}}
-    >
-      Already have an account?
-      <p>Log In</p>
-    </div>
-  {:else}
-    <div
-      on:click={() => {
-        register = true;
-      }}
-      on:keydown={() => {}}
-    >
-      Don't have an account? <p>Sign Up</p>
-    </div>
-    <div
-      on:click={() => {
-        authHandlers.resetPassword(email);
-      }}
-      on:keydown={() => {}}
-    >
-      Forgot Password?
-    </div>
-  {/if}
 </div>
 
 <style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  corePlugins: {
-    preflight: false,
-  },
   content: [
     './src/**/*.{svelte, html, js}',
   ],
   theme: {
-    colors: {
-      'grey': 'D9D9D9',
+    
+    extend: {
+      colors: {
+        'grey': 'D9D9D9',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
